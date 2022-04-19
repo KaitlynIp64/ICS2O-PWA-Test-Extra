5e5119ae34c289ff72a4e016defa284eb4f7b7b0
KaitlynIp64 <99354960+KaitlynIp64@users.noreply.github.com>
2022-04-19 02:15:21 +0000

    fixed js code

--- js/script.js
+++ js/script.js
@@ -10,8 +10,8 @@
  * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Test-1/sw.js", {
-    scope: "/ICS2O-PWA-Test-1/",
+  navigator.serviceWorker.register("/ICS2O-PWA-Test-Extra/sw.js", {
+    scope: "/ICS2O-PWA-Test-Extra/",
   })
 }
 
